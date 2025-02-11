69ee2de28419b858ba49a5f0ed212b217b3814ab
dylan-bloch <dylan.bloch@vikings.berry.edu>
2025-02-10 21:50:49 -0500

    changed db url

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
@@ -11,7 +12,9 @@ db_url = os.getenv("DATABASE_URL", "postgresql://postgres:[YOUR-PASSWORD]@db.twp
 if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)  # Render sometimes uses 'postgres://'
 
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url + "?sslmode=require"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = db_url + "?sslmode=require"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db_url = "postgresql://postgres:-Fxb.hy@U-6jQs@@db.twplccttrbfxpvafykqi.supabase.co:5432/postgres"
 
 db = SQLAlchemy(app)
